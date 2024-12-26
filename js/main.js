e293691bdb6d9b1d4b65aa9a71e882c01c4a69f5
ray <lthsiucutio@gmail.com>
2024-12-26 18:02:11 +0700

    Small edits/

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 document.querySelector('audio').volume = 0.4;
-        
+      
 const duration = 3 * 1000;
 const end = Date.now() + duration;
 
 (function frame() {
     confetti({
-        particleCount: 5,
+        particleCount: 3,
         startVelocity: 30,
         spread: 360,
         origin: { x: Math.random(), y: 0 },
-        colors: ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff']
+        colors: ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff'],
+        zIndex: 3 // Đặt z-index của confetti thấp hơn video nền
     });
 
     if (Date.now() < end) {
@@ -22,7 +23,8 @@ const end = Date.now() + duration;
                 startVelocity: 20,
                 spread: 360,
                 origin: { x: Math.random(), y: 0 },
-                colors: ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff']
+                colors: ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff'],
+                zIndex: 1 // Đặt z-index của confetti thấp hơn video nền
             });
         }, 500);
     }
