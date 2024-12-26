622e5f8b26b6e6ecedd977f388abca8af8408a78
ray <lthsiucutio@gmail.com>
2024-12-26 14:35:49 +0700

    Make the background audio louder

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
-document.querySelector('audio').volume = 0.1;
-
+document.querySelector('audio').volume = 0.4;
         
-        const duration = 3 * 1000;
-        const end = Date.now() + duration;
+const duration = 3 * 1000;
+const end = Date.now() + duration;
 
-        (function frame() {
+(function frame() {
+    confetti({
+        particleCount: 5,
+        startVelocity: 30,
+        spread: 360,
+        origin: { x: Math.random(), y: 0 },
+        colors: ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff']
+    });
+
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+    } else {
+        
+        setInterval(() => {
             confetti({
                 particleCount: 5,
-                startVelocity: 30,
+                startVelocity: 20,
                 spread: 360,
                 origin: { x: Math.random(), y: 0 },
                 colors: ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff']
             });
-
-            if (Date.now() < end) {
-                requestAnimationFrame(frame);
-            } else {
-                
-                setInterval(() => {
-                    confetti({
-                        particleCount: 5,
-                        startVelocity: 20,
-                        spread: 360,
-                        origin: { x: Math.random(), y: 0 },
-                        colors: ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff', '#a0c4ff', '#bdb2ff', '#ffc6ff']
-                    });
-                }, 500);
-            }
-        }());
+        }, 500);
+    }
+}());
