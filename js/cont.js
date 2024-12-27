f4936b16304ae9b04e108c35bebcdc0708dc9755
ray <lthsiucutio@gmail.com>
2024-12-28 01:16:58 +0700

    Bit

--- js/cont.js
+++ js/cont.js
@@ -1,4 +1,5 @@
 const video = new Request('../media/cont/background.webm');
+const au = new Request('../media/cont/thucgiac.mp3');
 var audio =  document.getElementById('background-audio');
 audio.volume = 0.4;
 fetch(video).then(response => {
