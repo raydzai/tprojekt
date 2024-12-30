cc4b9d6c185dc236c04ea647f5000fe5be2aff99
ray <lthsiucutio@gmail.com>
2024-12-31 02:09:31 +0700

    Big update!!

--- js/rewatch.js
+++ js/rewatch.js
@@ -7,3 +7,10 @@ if (/Mobi|Android|iPhone|iPad|iPod/.test(navigator.userAgent)) {
     document.body.style.margin = "0";
     document.body.innerHTML = "<h1 style='color: black; text-align: center;'>Truy cập từ thiết bị di động không được phép.</h1>";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".starter, #video-container");
+    elements.forEach(el => {
+        el.classList.add("visible");
+    });
+});
